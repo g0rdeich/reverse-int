cc9323bc95117d22bd109666bc69914885bff1a2
Alex Gordeev <gordeeffff@gmail.com>
2020-10-05 18:42:55 +0300

    solve reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let res = n.toString().split('').reverse().join('');
+    if(res[res.length-1] === '-') {
+        res = res.slice(0, res.length-1);
+    }
+    if(res[res.length-1] === '0') {
+        res = res.slice(1, res.length);
+    }
+    return res;
 }
